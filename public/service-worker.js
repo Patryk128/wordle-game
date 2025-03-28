47d4b66374be96b85a4cd10fcd0a5ae22229b459
Patryk Krawczyk <patrykkrawczykk157@gmail.com>
2025-03-28 22:23:37 +0100

    pwa

---
+++ public/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "wordle-pwa-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/assets",
+  "/sounds/win.mp3",
+  "/sounds/lose.mp3",
+  "/sounds/error.mp3",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
